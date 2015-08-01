093507b22f23c0f4ab8748dda624f8c43ea32936
David VanDusen <github@davidvandusen.com>
2015-08-01 16:11:22 -0700

    Add inspection spec.

--- spec/freight_spec.rb
+++ spec/freight_spec.rb
@@ -6,4 +6,9 @@ describe Freight do
     expect(freight.weight).to eq(500)
   end
 
+  it 'starts off with #inspection_passed? being false' do
+    freight = Freight.new('qwerty', 600)
+    expect(freight.inspection_passed?).to eq(false)
+  end
+
 end
