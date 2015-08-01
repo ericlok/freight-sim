65083ae072d99dc90bcd494a64b866ed7185e090
David VanDusen <github@davidvandusen.com>
2015-08-01 16:45:59 -0700

    Make industrial harder.

--- spec/industrial_freight_spec.rb
+++ spec/industrial_freight_spec.rb
@@ -2,6 +2,8 @@ describe IndustrialFreight do
 
   it 'must have its inspection completed off-site' do
     freight = IndustrialFreight.new('lorem', 900)
+    expect(freight.cargo_id).to eq('lorem')
+    expect(freight.weight).to eq(900)
     expect(freight.inspection_passed).to eq(true)
   end
 
