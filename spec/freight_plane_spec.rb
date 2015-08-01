7b3cb862399ec9ea8b2af59d2b4b441f8fb8e182
David VanDusen <github@davidvandusen.com>
2015-08-01 15:00:45 -0700

    Add first capacity spec.

--- spec/freight_plane_spec.rb
+++ spec/freight_plane_spec.rb
@@ -4,4 +4,8 @@ describe FreightPlane do
     expect(FreightPlane).to be_a(Class)
   end
 
+  it 'has a capacity that can vary from plane to plane' do
+    expect { FreightPlane.new(10) }.to_not raise_error
+  end
+
 end
