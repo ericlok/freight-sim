f853da4a86897b64b8d3b8cbcb5c7fe2849d97cc
David VanDusen <github@davidvandusen.com>
2015-08-02 09:52:08 -0700

    Add flight plan spec.

--- spec/flight_plan_spec.rb
+++ spec/flight_plan_spec.rb
@@ -1,3 +1,13 @@
 describe FlightPlan do
 
+  describe '.city_at' do
+
+    it 'returns the city for the given route and hop' do
+      expect(FlightPlan.city_at(0, 0)).to eq('Vancouver')
+      expect(FlightPlan.city_at(1, 2)).to eq('Seattle')
+      expect(FlightPlan.city_at(2, 2)).to eq('Orlando')
+    end
+
+  end
+
 end
